123faacae5bc73b2994363888c582100690d1eb9
Abel Ianovici <ianovici.abel04@gmail.com>
2026-08-01 23:17:05 +0200

    Design Changes

--- js/base.js
+++ js/base.js
@@ -1,1 +1,14 @@
 document.body.classList.remove('no-js'); // Removes fallback styling 
+
+const header = document.getElementById("top");
+const button = document.getElementById("top-btn");
+
+button.style.display = "none";
+
+window.addEventListener("scroll", () => {
+    if (window.scrollY > header.offsetHeight) {
+        button.style.display = "block";
+    } else {
+        button.style.display = "none";
+    }
+});
